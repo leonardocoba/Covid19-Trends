27635ae2933bb5379b4d88d1ee39b5de7d41de39
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2024-04-05 12:35:56 -0400

    Updated Size of queires

--- src/layouts.py
+++ src/layouts.py
@@ -19,31 +19,30 @@ login_page = html.Div(
     className="login-page-container"
 )
 
-
-home_page = html.Div(style={'display': 'flex'}, children=[
-    html.Div(style={'flex': 1}, children=[
-        html.H2('Filters'),
-        html.Label('Location:'),
+home_page = html.Div(style={'display': 'flex', 'width': '100%'}, children=[
+    html.Div(style={'width': '25%', 'font-size': '16px'}, children=[
+        html.H2('Filters', style={'font-size': '20px'}),
+        html.Label('Location:', style={'font-size': '18px'}),
         dcc.Dropdown(id='location-filter', options=[
             {'label': country, 'value': country} for country in query_obj.data['Country'].unique()
         ], multi=True),
-        html.Label('Gender:'),
+        html.Label('Gender:', style={'font-size': '18px'}),
         dcc.Checklist(id='gender-filter', options=[
             {'label': 'Male', 'value': 'Male'},
             {'label': 'Female', 'value': 'Female'}
         ], value=['Male', 'Female']),
-        html.Label('Age:'),
+        html.Label('Age:', style={'font-size': '18px'}),
         dcc.RangeSlider(id='age-filter', min=1, max=100, value=[1, 100], marks={i: str(i) for i in range(0, 101, 10)}),
-        html.Label('Mortality:'),
+        html.Label('Mortality:', style={'font-size': '18px'}),
         dcc.Checklist(id='mortality-filter', options=[
             {'label': 'Infected', 'value': 'Infected'},
             {'label': 'Deaths', 'value': 'Deaths'}
         ], value=['Infected', 'Deaths']),
     ]),
-    html.Div(style={'flex': 2}, children=[
-        html.H1('Tuberculosis Trends'),
+    html.Div(style={'width': '75%'}, children=[
+        html.H1('Tuberculosis Trends', style={'font-size': '24px'}),
         dcc.Graph(id='tuberculosis-graph'),
-        html.H3(id='total-count'),
-        html.P(id='graph-description'),
+        html.H3(id='total-count', style={'font-size': '20px'}),
+        html.P(id='graph-description', style={'font-size': '16px'}),
     ]),
 ])
