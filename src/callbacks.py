79d6a50f1c87e64029d84eaa12da8cdbb1505c75
Leonardo D. Cobaleda <116767849+leonardocoba@users.noreply.github.com>
2024-04-14 15:12:33 -0400

    Removed query object. Integration SQL needed

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,10 +1,8 @@
 from dash import Input, Output, State, callback
 from app import app
-from query import QueryObject
 import pandas as pd
 import plotly.express as px
 
-query_obj = QueryObject()
 
 @app.callback(Output('url', 'pathname'), 
               [Input('login-button', 'n_clicks')],
@@ -22,26 +20,20 @@ def login(n_clicks, username, password):
      Input('age-filter', 'value'), Input('mortality-filter', 'value')]
 )
 def update_graph_and_text(location, gender, age, mortality):
-    filtered_data = query_obj.filter_data(gender=gender, age_range=age, country=location, mortality_status=mortality)
-
-    proportions = []
-    for country in filtered_data['Country'].unique():
-        for gen in ['Male', 'Female']:
-            for status in ['Infected', 'Deaths']:
-                subset = filtered_data[(filtered_data['Country'] == country) & (filtered_data['Gender'] == gen) & (filtered_data['Mortality Status'] == status)]
-                proportions.append({'Country': country, 'Gender': gen, 'Mortality Status': status, 'Proportion': len(subset)})
-
-    proportions_df = pd.DataFrame(proportions)
+    # Creating an empty DataFrame with the structure needed for the plot
+    proportions_df = pd.DataFrame(columns=['Country', 'Gender', 'Mortality Status', 'Proportion'])
+    
+    # Creating an empty plot using the empty DataFrame
     fig = px.bar(proportions_df, x='Country', y='Proportion', color='Mortality Status', 
                  barmode='stack', facet_col='Gender', title='Tuberculosis Data Visualization',
                  category_orders={"Gender": ["Male", "Female"], "Mortality Status": ["Infected", "Deaths"]})
-
-    total_count = f"Total Count: {len(filtered_data)}"
-    selected_avg_age = query_obj.calculate_average_age(filtered_data)
-    complementary_avg_age = query_obj.calculate_complementary_average_age(gender=gender, country=location, mortality_status=mortality)
-    description = f"The average age for the selected group is {selected_avg_age:.2f}. Compared to the complementary group, which has an average age of {complementary_avg_age:.2f}."
+    
+    # Setting static text as there's no data to count or describe
+    total_count = "Total Count: 0"
+    description = "The average age for the selected group is N/A. Compared to the complementary group, which has an average age of N/A."
 
     return fig, total_count, description
+
 
 """# Define a function to retrieve data from Oracle and update the graph
 def update_graph():
